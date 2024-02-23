57737f972bb20ff49b7dbe13894c2ff31549c66f
nalinally <obama1632@gmail.com>
2024-02-23 09:54:32 +0900

    modified readme

---
+++ codes/mqtt_py/sample_sub.py
@@ -0,0 +1,56 @@
+# python3.6
+
+import random
+from paho.mqtt import client as mqtt_client
+import time
+
+
+broker = 'broker.emqx.io'
+port = 1883
+client_id = f'python-mqtt-{random.randint(0, 100)}'
+
+
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def publish(client, topic):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+
+def subscribe(client: mqtt_client, topic):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client, "/temp")
+    subscribe(client, "/temp")
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
